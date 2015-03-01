6d7e68615a50dd6fc4d138a895e7892e67da459b
arancha <wcqueaaa@163.com>
2015-03-01 11:50:16 +0800

    kf.php add done

--- kf.php
+++ kf.php
@@ -1,7 +1,13 @@
 <?php
 
-$urlTpl  = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=%s";
-$addid_data = json_encode(array('kf_account':'yf@izhuanche','nickname':'yf','password':'www123'));
-list($return_code, $return_content) = http_post_data($url, $data);
+define($acc_token, "n27iREFxCCqHJjhY-g2sYzMXXXt2y_YEEo82PMT7HGtZ3BJGLf0Ly1b8_g5TeB0gtVvCexcHsBmtXuLo9KQ83o3jm0nIp1JTlFgT3Nlpads");
+$urlTpl  = "https://api.weixin.qq.com/customservice/kfaccount/add?access_token=%s";
+$url = sprintf($urlTpl, $acc_token);
+$addid_data = json_encode(array('kf_account':'yf@izhuanche','nickname':'yf','password':'www123456'));
+list($return_code, $return_content) = http_post_data($url, $addid_data);
+
+$fp = fopen("php.log","a+");
+fwrite($fp, "return_code[$return_code] return_content[$return_content]\n");
+fclose($fp);
 
 ?>

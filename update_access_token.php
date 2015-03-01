09afc97d22661510bc62b692ba2c287011ba85f9
arancha <wcqueaaa@163.com>
2015-03-01 13:46:45 +0800

    ...

--- update_access_token.php
+++ update_access_token.php
@@ -7,5 +7,7 @@ $secret = "97aaf6be50e8d70c509d98aedae66098";
 $urlTpl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s";
 $url = sprintf($urlTpl, $appid, $secret);
 
-echo http_get_data($url);
+$token_json = http_get_data($url);
+$token_obj = json_decode($token_json);
+echo $token_obj->{'access_token'};
 ?>
